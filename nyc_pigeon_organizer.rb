b5427d0f0007aaf313d64e6c650b9c3093172261
gs2589 <gabriel.seidner@gmail.com>
2016-03-22 13:05:16 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+
+new_hash={}
+
+  data.each do |attribute, alleles_hash|
+    alleles_hash.each do |allele, name_arr| 
+      name_arr.each do |name|
+
+      ((new_hash[name]||={}) [attribute] ||=[]) << allele.to_s
+
+
+    end
+  end
+
+  end
+  new_hash
 end
